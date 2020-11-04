79deb2ff5f75c171c1f316f32a9f5f6de4f4906c
Carvel Saputra <carvel.saputra@gmail.com>
2020-11-04 14:16:00 +0700

    init project
    pembuatan project untuk soal nomor 1
    Seleksi UKM dengan tampilan linier layout

---
+++ SeleksiUKM/app/src/main/java/com/ti5a/seleksiukm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ti5a.seleksiukm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
