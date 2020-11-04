e0eee6f25e26709bfd43701f0270791c57ae484e
Carvel Saputra <carvel.saputra@gmail.com>
2020-11-04 17:06:57 +0700

    final
    nomor 4 selesai menggunakan url

--- SearchingArea/app/src/main/java/com/ti5a/searchingarea/MainActivity.java
+++ SearchingArea/app/src/main/java/com/ti5a/searchingarea/MainActivity.java
@@ -2,13 +2,32 @@ package com.ti5a.searchingarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_cari;
+    EditText et_lokasi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_cari = findViewById(R.id.btn_cari);
+        et_lokasi = findViewById(R.id.et_lokasi);
+
+        btn_cari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = et_lokasi.getText().toString();
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q="+url);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 }
